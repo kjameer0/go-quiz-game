4e4068c266dc4b1b1fad3c188bc5b583e7a76875
Khalid Jameer <41968688+kjameer0@users.noreply.github.com>
2024-08-25 11:10:20 -0400

    fix: added bufio package to wait for Enter key from user

--- index.go
+++ index.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"sync"
 	"time"
+	"bufio"
 )
 
 func getFileText(userPath string) (string, error) {
@@ -19,43 +20,60 @@ func getFileText(userPath string) (string, error) {
 	return string(file), nil
 }
 
-func handleTimeEnd(timer *time.Timer, wg *sync.WaitGroup) {
+func handleTimeEnd(wg *sync.WaitGroup) {
 	defer wg.Done()
+	timer := time.NewTimer(time.Duration(3) * time.Second)
 	<-timer.C
 	fmt.Println("Time is up for the quiz")
 	os.Exit(0)
 }
+
 func main() {
-	fmt.Print("Press Enter to begin quiz")
-	fmt.Scanf("n")
+	fmt.Println("Press Enter to continue...")
+
+	// Create a new reader from standard input
+	reader := bufio.NewReader(os.Stdin)
+
+	// Read a line of input until Enter is pressed
+	// This method is waiting for a user to submit some text
+	_, _ = reader.ReadString('\n')
+	fmt.Print("Started\n")
+
 	var wg sync.WaitGroup
 	wg.Add(1)
-	timer := time.NewTimer(time.Duration(3) * time.Second)
-	go handleTimeEnd(timer, &wg)
-	filetext, err := getFileText("problems.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
+
+	go handleTimeEnd(&wg)
+
 	go func() {
 		defer wg.Done()
-		r := csv.NewReader(strings.NewReader(filetext))
+		filetext, err := getFileText("problems.csv")
+		if err != nil {
+			log.Fatal(err)
+		}
 
+		r := csv.NewReader(strings.NewReader(filetext))
 		records, err := r.ReadAll()
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		var correctAnswers int = 0
 		for i := 0; i < len(records); i += 1 {
 			var curQuestion string = records[i][0]
 			var curAnswer string = records[i][1]
 			var userAnswer string
 			fmt.Println(curQuestion)
-			fmt.Scan(&userAnswer)
+			_, err := fmt.Scan(&userAnswer)
+			if err != nil {
+				log.Fatal("Something went wrong")
+			}
+			fmt.Println(userAnswer)
 			if userAnswer == curAnswer {
 				correctAnswers += 1
 			}
 		}
 		fmt.Printf("You got %v out of %v\n", correctAnswers, len(records))
 	}()
+
 	wg.Wait()
 }
